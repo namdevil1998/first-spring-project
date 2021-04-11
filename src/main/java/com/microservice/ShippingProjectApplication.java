f43dfa202fab46d6c604416d504c775f273ece27
namdevil1998 <62322817+namdevil1998@users.noreply.github.com>
2021-04-11 15:36:33 +0700

    security oke

--- src/main/java/com/microservice/ShippingProjectApplication.java
+++ src/main/java/com/microservice/ShippingProjectApplication.java
@@ -3,7 +3,7 @@ package com.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.microservice"})
 public class ShippingProjectApplication {
 
     public static void main(String[] args) {
